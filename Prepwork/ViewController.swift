70cb9ae6d8a6b89c4bdbc7e9b80da420655a75da
thibui0908 <thi.t.bui@sjsu.edu>
2021-01-26 23:23:22 -0800

    Initial Commit

---
+++ Prepwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prepwork
+//
+//  Created by Thi Bui on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
